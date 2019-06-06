08b360bb796c44ca699e16d0ea125b0e411f2d09
Alok Rajasukumaran <alokraj68@gmail.com>
2019-06-06 20:22:52 +0530

    initial check in
    check in

---
+++ test.py
@@ -0,0 +1,13 @@
+def g():
+  global big
+  big = open('big.txt').read()
+  N = len(big)
+  s = set()
+  for i in range(6, N):
+    c = big[i]
+    if ord(c) > 127 and c not in s:
+        print (i, c, ord(c), big[max(0, i-10):min(N, i+10)])
+        s.add(c)
+  print (s)
+  print ([ord(c) for c in s])
+g()
